34c13aa5119499de780f713dc9352497c730a389
Nunu-0 <66766470+Nunu-0@users.noreply.github.com>
2022-02-10 16:51:11 +0900

    Ch01: windowCoordnations

--- Practice/02.window_coordnations.js
+++ Practice/02.window_coordnations.js
@@ -1,10 +1,22 @@
 'use strict';
 
-
 function pink_click(){
     const pink = document.querySelector(".pink");
     const client = pink.getBoundingClientRect();
     const scrollY = pageYOffset;
     console.log(`clinet: ${client.top}, ${client.left}`);
     console.log(`page: ${client.top + scrollY}, ${client.left}`);
+};
+
+function upBtn_click(){
+    window.scrollTo({top: 0, left: 0, behavior:'smooth'})
+};
+
+function down100Btn_click(){
+    window.scrollBy(0, 150);
+}
+
+function goPink_click(){
+    const pink = document.querySelector(".pink");
+    pink.scrollIntoView();
 };

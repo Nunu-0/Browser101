796fbc6a7bebed1b15ff66757579578c1d498853
Nunu-0 <66766470+Nunu-0@users.noreply.github.com>
2022-02-14 16:53:36 +0900

    Ch02: coordinates007_Performance_Update

--- Ch02. WebAPIs-Game/coordinates007.js
+++ Ch02. WebAPIs-Game/coordinates007.js
@@ -5,25 +5,30 @@ const img = document.querySelector('img');
 const columnLine = document.querySelector('.columnLine');
 const rowLine = document.querySelector('.rowLine');
 
-window.addEventListener('mousemove', (e)=>{;
-    const x = e.clientX;
-    const y = e.clientY;
-    let viewX = 20;
-    let viewY = 20;
+addEventListener('load', () =>{
+    const targetRect = img.getBoundingClientRect();
+    console.log(targetRect);
+    const targetHalfWidth = targetRect.width /2;
+    const targetHalfHeight = targetRect.height /2;
 
-    img.style.top= `${y}px`;
-    img.style.left= `${x}px`;
-    columnLine.style.top= `${y}px`;
-    rowLine.style.left= `${x}px`;
-    coordView.textContent = `${x} x ${y}`;
-    coordView.style.top= `${y}px`;
-    coordView.style.left= `${x}px`;
-    if (x+110 > window.innerWidth){
-        viewX= -120;
-    }else viewX= 20;
-    if(y+70 > window.innerHeight){
-        viewY= -40;
-    }
-    else viewY= 20;
-    coordView.style.transform= `translate(${viewX}px, ${viewY}px)`;
-});
+    window.addEventListener('mousemove', (e)=>{;
+        const x = e.clientX;
+        const y = e.clientY;
+        let viewX = 20;
+        let viewY = 20;
+
+        rowLine.style.transform= `translateX(${x}px)`;
+        columnLine.style.transform= `translateY(${y}px)`;
+        img.style.transform = `translate(${x - targetHalfWidth}px, ${y - targetHalfHeight}px)`;
+        coordView.style.transform = `translate(${x}px, ${y}px)`;
+        coordView.textContent = `${x} x ${y}`;
+        if (x+110 > window.innerWidth){
+            viewX= -120;
+        }else viewX= 20;
+        if(y+70 > window.innerHeight){
+            viewY= -40;
+        }
+        else viewY= 20;
+        coordView.style.transform= `translate(${x+viewX}px, ${y+viewY}px)`;
+    });
+})
